26f2d31d311a66aaee066ef76dd0039127e5de81
Anna Filin <68apple@gmail.com>
2025-06-02 15:44:52 +0300

    Initial Commit

---
+++ CocktailExplorer/CocktailExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailExplorerApp.swift
+//  CocktailExplorer
+//
+//  Created by Anna Filin on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CocktailExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
